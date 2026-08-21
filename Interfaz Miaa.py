cba239a57022a07f07f53b0453c8e900e5c97965
Miaa <pedroguillermomiaa@gmail.com>
2026-08-21 09:33:55 -0600

    Update Interfaz Miaa.py

--- Interfaz Miaa.py
+++ Interfaz Miaa.py
@@ -17,7 +17,7 @@ if vista_actual != 'home':
     css_ancho_total = """
     /* Forzar ancho completo de pantalla para las aplicaciones embebidas */
     .block-container {
-        max-width: 90% !important;
+        max-width: 80% !important;
         padding-left: 0rem !important;
         padding-right: 0rem !important;
         padding-top: 0.5rem !important;
